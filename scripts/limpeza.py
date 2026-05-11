d39a5c0fe88c56ce2e89ae6c65dc773e83826d12
Matheus <178436024+maxmati1@users.noreply.github.com.>
2026-05-11 15:55:27 -0300

    Implementando limpeza e padronizacao dos dados

--- scripts/limpeza.py
+++ scripts/limpeza.py
@@ -5,3 +5,27 @@ arquivo = pd.read_csv('dados/vendas.csv')
 print('\nBASE ORIGINAL\n')
 
 print(arquivo)
+
+print('\n========================')
+print('INICIANDO LIMPEZA')
+print('========================\n')
+
+duplicados = arquivo.duplicated().sum()
+
+arquivo = arquivo.drop_duplicates()
+
+print(f'Duplicados removidos: {duplicados}')
+
+valores_vazios = arquivo.isnull().sum().sum()
+
+print(f'Valores vazios encontrados: {valores_vazios}')
+
+arquivo['cidade'] = arquivo['cidade'].fillna('Nao informado')
+
+arquivo['cidade'] = arquivo['cidade'].str.title()
+
+valores_negativos = (arquivo['valor'] < 0).sum()
+
+print(f'Valores negativos encontrados: {valores_negativos}')
+
+arquivo.loc[arquivo['valor'] < 0, 'valor'] = 0
